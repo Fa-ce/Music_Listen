06f19f084b9e1955c1b024c7f5c10c87ba05c8b5
Fa-ce <3051810772@qq.com>
2022-04-15 10:29:10 +0800

    更新请求接口

--- src/network/request.js
+++ src/network/request.js
@@ -1,10 +1,8 @@
 import axios from 'axios'
 import $store from '@/store/index'
 
-// const baseURL = "https://www.yangaoyuan.cn";
-// const baseURL = 'https://autumnfish.cn/'
 const baseURL = "http://localhost:3000";
-// const baseURL = "http://iwenwiki.com:3000/";
+
 
 let ajaxTimer = 0;
 export function request(config) {
